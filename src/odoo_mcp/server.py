a545088499fd67194ea4c5f11782b1508d66f6b3
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2025-04-11 14:25:06 +0700

    fix search_read

--- src/odoo_mcp/server.py
+++ src/odoo_mcp/server.py
@@ -207,7 +207,7 @@ def search_record(
     """
     try:
         odoo_client = get_odoo_client()
-        results = odoo_client.search_read(model_name, domain, fields)
+        results = odoo_client.execute_method(model_name, 'search_read', domain, fields=fields)
         _logger.info(results)
         return {"success": True, "result": results}
     except Exception as e:
